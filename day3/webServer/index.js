494d027522eeea41a5af33ac980401bd3c2e83a9
heyaman003 <heyaman329@gmail.com>
2023-07-01 01:53:53 +0530

    understand basics concept of web server

--- day3/webServer/index.js
+++ day3/webServer/index.js
@@ -1,11 +1,18 @@
 const http=require('http');
+
+const fs=require('fs');
+const path = require("path");
+const filePath = path.join(__dirname, "index.html");
+const outpt = fs.readFileSync(filePath, "utf-8");
 const data={age:'20',name:"aman"}
 const server=http.createServer((req,res)=>{
         console.log("hello don")
         console.log(req.url);
         console.log(req.statusCode);//why it's consoling null instead of 200?
-        res.setHeader('Content-Type','application/json');
-        res.end(JSON.stringify(data));
+        res.setHeader('Content-Type','text/html');
+       //res.end(JSON.stringify(data));
+       res.end(outpt);
+
         
 }
       
